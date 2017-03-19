eef3527d17ce148c927be8177481723c76073128
Vicky McDermott <vickymmcd@gmail.com>
2017-03-19 19:08:44 -0400

    Finishing my flask toolbox

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,29 @@
-"""
-Put your Flask app code here.
-"""
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/result/', methods=['POST', 'GET'])
+def result():
+    error = None
+    if request.method == 'POST':
+        result = request.form
+        for key, val in result.items():
+            if key == 'name':
+                name = val
+            elif key == 'age':
+                age = val
+            elif key == 'favninja':
+                ninja = val
+        if name == '' or age == '' or ninja == '':
+            error = 'Please fill in all fields.'
+    return render_template("result.html", name=name, age=age,
+                           ninja='Patrick Huston', error=error)
+
+
+if __name__ == '__main__':
+    app.run()
