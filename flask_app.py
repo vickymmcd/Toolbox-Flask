3c37678a285fb8d637daa54a8b853027f2283a09
Vicky McDermott <vickymmcd@gmail.com>
2017-04-12 14:43:03 -0400

    changing host and port to make website work

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os.environ
 app = Flask(__name__)
 
 
@@ -26,4 +27,6 @@ def result():
 
 
 if __name__ == '__main__':
-    app.run()
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+    PORT = int(os.environ.get('PORT', 5000)))
+    app.run(host=HOST, port=PORT)
