4b097a7e67cd7577bd76f045ace33c6f741c4752
Vicky McDermott <vickymmcd@gmail.com>
2017-04-12 21:51:03 -0400

    trying again

--- flask_app.py
+++ flask_app.py
@@ -28,5 +28,5 @@ def result():
 
 if __name__ == '__main__':
     HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-    PORT = int(os.environ.get('PORT', 5000)))
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host=HOST, port=PORT)
